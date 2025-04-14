8db3facaa255ee55e6cac249eb238aadcfd588de
aravindav <aravindwaitz@gmail.com>
2025-04-14 04:31:42 -0400

    Added ForShellScript.swift to the repo

---
+++ ForShellScript.swift
@@ -0,0 +1,12 @@
+//
+//  ForShellScript.swift
+//  CounterApp
+//
+//  Created by Aravind on 14/04/25.
+//
+
+import Foundation
+
+func sampleFunction() {
+    print("This is sample file created for shell script")
+}
